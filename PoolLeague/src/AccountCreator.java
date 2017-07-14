0781edafb207f3d8789636dbf9d2ccb1d0e8c5f0
Aidan Yeaman <aidan.93@live.co.uk>
2017-07-14 23:28:07 +0100

    Updated file
    Removed earlier demo tests that were commented out.

--- PoolLeague/src/AccountCreator.java
+++ PoolLeague/src/AccountCreator.java
@@ -44,51 +44,7 @@ public class AccountCreator {
 		
 		station.getListOfTeam();
 		station.getNameOfPlayersInList();
-//		accCr8.setTypeOfAccount(2);
-//		station.addPlayerToTeam(accCr8.getTypeOfAccount());
-//		
-//		accCr8.setTypeOfAccount(2);
-//		station.addPlayerToTeam(accCr8.getTypeOfAccount());
-//		
-//		accCr8.setTypeOfAccount(0);
-//		station.addPlayerToTeam(accCr8.getTypeOfAccount());
-//		
-//		accCr8.setTypeOfAccount(1);
-//		station.addPlayerToTeam(accCr8.getTypeOfAccount());
-//	
-//		station.getListOfTeam();
-		
-//		station.addPlayerToTeam(p1);
-//		station.addPlayerToTeam(ch1);
-//		station.addPlayerToTeam(c1);
-//		station.getListOfTeam();
-		
-		
-		
-		
-//		p1.displayDetails();
-//		c1.displayDetails();
-//		ch1.displayDetails();
-//		c1.setEmail(null);
-//		c1.setPassword(null);
-//		c1.setType(c1);
-//		
-//		ch1.setEmail(null);
-//		ch1.setPassword(null);
-//		ch1.setType(ch1);
-//		
-//		p1.getEmail();			
-//		p1.getPassword();	
-//		p1.getType();
-//		
-//		c1.getEmail();
-//		c1.getPassword();
-//		c1.getType();
-//		
-//		ch1.getEmail();
-//		ch1.getPassword();
-//		ch1.getType();
-		
+
 		
 
 	}
